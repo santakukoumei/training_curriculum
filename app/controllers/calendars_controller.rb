5d1cd96702ec83b003a1de38aed6a4de6ff518f9
santakukoumei <rx0gtrsv@gmail.com>
2021-11-11 22:21:15 +0900

    Issue3

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -30,7 +30,7 @@ class CalendarsController < ApplicationController
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
-      Today_plans = []
+      today_plans = []
       plans.each do |plan|
         Today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
